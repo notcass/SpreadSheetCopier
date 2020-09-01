39bf1ae93c2c3e2daeaa6d848a349628549040ea
Cass Bates <cassbates@gmail.com>
2020-08-31 23:33:54 -0500

    Cleaning

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 
@@ -19,7 +18,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("mainScene.fxml"));
         primaryStage.setResizable(false);
         primaryStage.setTitle("Spreadsheet Copier");
         primaryStage.setScene(new Scene(root, 600, 500));
